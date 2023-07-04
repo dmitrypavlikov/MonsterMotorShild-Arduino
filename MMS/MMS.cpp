bcaa01f763a1b2d1b819610f0a76a7aeabd0699f
Dmitry <56763340+dmitrypavlikov@users.noreply.github.com>
2023-07-04 17:53:54 +0300

    Need to clear trash

--- MMS/MMS.cpp
+++ MMS/MMS.cpp
@@ -11,35 +11,36 @@
 
 
 // Left motor
-#define DIR1A   2   // Direction CW
-#define DIR2A   3   // Direction CWW 1B
-#define PWM1    4   // Speed (0-255)
-#define TRQ1    A0  // #IF PINMODE=OUTPUT# Torque (HIGH=Enable)
-#define DIAG1   A0  // #IF PINMODE=INPUT#  Diagnostic (HIGH=NO_PROBLEM)
-#define CUR1    A2  // Present Current (0-1023) (0-65 mA)
+#define DIR1A   2           // Direction CW
+#define DIR2A   3           // Direction CWW
+#define PWM1    4           // Speed (0-255)
+#define TRQ1    A0          // #IF PINMODE=OUTPUT# Torque (HIGH=Enable)
+#define DIAG1   A0          // #IF PINMODE=INPUT#  Diagnostic (HIGH=NO_PROBLEM)
+#define CUR1    A2          // Present Current (0-1023) (0-65 mA)
 //Left encoder
-#define EN1A  10   // Trigger of Interrupt
-#define EN2A  11   // 
-#define TIX_PER_SPIN_L 496
+#define EN1A  10            // Trigger of Interrupt for left encoder
+#define EN2A  11            // Second pin of left encoder
+#define TIX_PER_SPIN_L 496  // Ticks per one spin
+
 
 // Right motor
-#define DIR1B   5   // Direction CW 1B
-#define DIR2B   6   // Direction CWW 
-#define PWM2    7   // Speed (0-255)
-#define TRQ2    A1  // #IF PINMODE=OUTPUT# Torque (HIGH=Enable)
-#define DIAG2   A1  // ##IF PINMODE=INPUT#  Diagnostic (HIGH=NO_PROBLEM)
-#define CUR2    A3  // Present Current (0-1023) (0-65 mA)
+#define DIR1B   5           // Direction CW
+#define DIR2B   6           // Direction CWW 
+#define PWM2    7           // Speed (0-255)
+#define TRQ2    A1          // #IF PINMODE=OUTPUT# Torque (HIGH=Enable)
+#define DIAG2   A1          // ##IF PINMODE=INPUT#  Diagnostic (HIGH=NO_PROBLEM)
+#define CUR2    A3          // Present Current (0-1023) (0-65 mA)
 // Right encoder 
-#define EN1B  13  // 
-#define EN2B  12  // Trigger of Interrupt
-#define TIX_PER_SPIN_R 696
+#define EN1B  13            // Srcond pin of right encoder
+#define EN2B  12            // Trigger of Interrupt for right encoder
+#define TIX_PER_SPIN_R 696  // Ticks per one spin
+
 
 // Robot settings
-#define BASE_WIDTH 0.315
-#define MAX_CURRENT 10  //TODO or not use
-#define MAX_VEL 3.5
-#define WHEEL_DIAMETER 200 // мм/с
-
+#define BASE_WIDTH 0.315    // meter
+#define MAX_CURRENT 10      // NOT USED NOW
+#define MAX_VEL 3.5         // TODO!!!!
+#define WHEEL_DIAMETER 0.2  // meter
 
 
 
@@ -78,14 +79,10 @@ class MMS
     pinMode(9, OUTPUT);
     digitalWrite(8, HIGH);
     digitalWrite(9, LOW);
-
-    
-
     }
   
-  void clear(){
-  // EMPTY
-  }
+
+ 
 
   // Make wheels free for spinning by hands, zero speed
   void torque_disable(){
@@ -97,7 +94,11 @@ class MMS
   
   // Emergency STOP of motors by torque disable and zero spd
   // void brake_motors(){;}
- 
+  
+  void clear(){
+  // EMPTY
+  }
+
   // Set speed of motors by linear and angular velocity
   /*void set_vel(float linear, float angular){
     SPD_L = 0;
