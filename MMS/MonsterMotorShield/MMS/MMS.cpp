22b84757412ce56593686810ae878185d652dcb4
Dmitry <56763340+dmitrypavlikov@users.noreply.github.com>
2023-08-28 18:30:30 +0300

    Need to add odom pub

---
+++ MMS/MonsterMotorShield/MMS/MMS.cpp
@@ -0,0 +1,158 @@
+/*------BASE_CONTROL-----*/
+
+#include "Arduino.h"
+
+// PinChangeInterrupt Lib for Arduino: 
+// https://github.com/NicoHood/PinChangeInterrupt
+#include <PinChangeInterrupt.h>
+#include <PinChangeInterruptBoards.h>
+#include <PinChangeInterruptPins.h>
+#include <PinChangeInterruptSettings.h>
+
+
+// Left motor
+#define DIR1A   2           // Direction CW
+#define DIR2A   3           // Direction CWW
+#define PWM1    4           // Speed (0-255)
+#define TRQ1    A0          // #IF PINMODE=OUTPUT# Torque (HIGH=Enable)
+#define DIAG1   A0          // #IF PINMODE=INPUT#  Diagnostic (HIGH=NO_PROBLEM)
+#define CUR1    A2          // Present Current (0-1023) (0-65 mA)
+//Left encoder
+#define EN1A  10            // Trigger of Interrupt for left encoder
+#define EN2A  11            // Second pin of left encoder
+#define TIX_PER_SPIN_L 496  // Ticks per one spin
+
+
+// Right motor
+#define DIR1B   5           // Direction CW
+#define DIR2B   6           // Direction CWW 
+#define PWM2    7           // Speed (0-255)
+#define TRQ2    A1          // #IF PINMODE=OUTPUT# Torque (HIGH=Enable)
+#define DIAG2   A1          // ##IF PINMODE=INPUT#  Diagnostic (HIGH=NO_PROBLEM)
+#define CUR2    A3          // Present Current (0-1023) (0-65 mA)
+// Right encoder 
+#define EN1B  13            // Srcond pin of right encoder
+#define EN2B  12            // Trigger of Interrupt for right encoder
+#define TIX_PER_SPIN_R 696  // Ticks per one spin
+
+
+// Robot settings
+#define BASE_WIDTH 0.315    // meter
+#define MAX_CURRENT 10      // NOT USED NOW
+#define MAX_VEL 3.5         // TODO!!!!
+#define WHEEL_DIAMETER 0.2  // meter
+
+
+
+class MMS 
+{
+  public:
+  int CURRENT_L = 0;
+  int CURRENT_R = 0;
+  bool DIAG_L = true;
+  bool DIAG_B = true;
+
+
+  // All pinMode in one function
+  // IT DOES NOT WORK IN CPP FILE, ONLY IN INO FILE
+  void start(){
+    // Left motor
+    pinMode(PWM1,OUTPUT);
+    pinMode(DIR1A,OUTPUT);
+    pinMode(DIR2A,OUTPUT);
+    pinMode(DIAG1,INPUT);    // ATTENTION: Torque pin in Diagnostic workmode
+    pinMode(CUR1,INPUT);
+    // Left encoder
+    pinMode(EN1A, INPUT_PULLUP);
+    pinMode(EN2A, INPUT_PULLUP);
+
+    // Right motor
+    pinMode(PWM2,OUTPUT);
+    pinMode(DIR1B,OUTPUT);
+    pinMode(DIR2B,OUTPUT);
+    pinMode(DIAG2,INPUT);   // ATTENTION: Torque pin in Diagnostic workmode
+    pinMode(CUR2,INPUT);
+    // Right encoder
+    pinMode(EN1B, INPUT_PULLUP);
+    pinMode(EN2B, INPUT_PULLUP);
+
+    //GPIO Pins for power supply of right encoder
+    pinMode(8, OUTPUT);
+    pinMode(9, OUTPUT);
+    digitalWrite(8, HIGH);
+    digitalWrite(9, LOW);
+  }
+ 
+  // Stop motors when linear and angular velocities = 0.0
+  // IT DOES NOT WORK IN CPP FILE, ONLY IN INO FILE
+  void motor_stop(){
+    analogWrite(PWM1, 0);  
+    analogWrite(PWM2, 0);
+    digitalWrite(DIR1A,LOW); digitalWrite(DIR2A,LOW);
+    digitalWrite(DIR1B,LOW); digitalWrite(DIR2B,LOW);
+    digitalWrite(DIR1A,HIGH); digitalWrite(DIR2A,HIGH);
+    digitalWrite(DIR1B,HIGH); digitalWrite(DIR2B,HIGH);
+  }
+
+  // Make wheels free for spinning by hands, zero speed
+  // IT DOES NOT WORK IN CPP FILE, ONLY IN INO FILE
+  void torque_disable(){
+    analogWrite(PWM1, 0);  
+    analogWrite(PWM2, 0);
+    digitalWrite(DIR1A,LOW); digitalWrite(DIR2A,LOW);
+    digitalWrite(DIR1B,LOW); digitalWrite(DIR2B,LOW);
+  }
+  
+  void clear(){
+  // EMPTY
+  }
+
+  void set_pwm(float SPD_L, float SPD_R){
+    // Potential protection
+    if((SPD_L == 0.0)&&(SPD_R == 0.0)){
+      // DONT WORK IN CPP FILE, ONLY IN INO FILE
+      motor_stop();
+      //torque_disable();
+    }
+
+    // Set direction of spinning for each wheels
+    if(SPD_L > 0.0) {
+      digitalWrite(DIR1A,HIGH); digitalWrite(DIR2A,LOW);
+    }
+    if(SPD_L < 0.0) {
+      digitalWrite(DIR1A,LOW); digitalWrite(DIR2A,HIGH);
+    }
+    if(SPD_R > 0.0) {
+      digitalWrite(DIR1B,HIGH); digitalWrite(DIR2B,LOW);
+    }
+    if(SPD_R < 0.0) {
+      digitalWrite(DIR1B,LOW); digitalWrite(DIR2B,HIGH);
+    }
+
+    // Protection by MAX_VELOCITY
+    /*if(SPD_L >= MAX_VEL){
+      SPD_L = MAX_VEL;
+    } else if(SPD_R >= MAX_VEL){
+      SPD_R = MAX_VEL;
+    }
+    if(SPD_L <= -MAX_VEL){
+      SPD_L = -MAX_VEL;
+    } else if(SPD_R <= -MAX_VEL){
+      SPD_R = -MAX_VEL;
+    }*/
+    // Set PWM speed to each wheels
+    analogWrite(PWM1, abs(SPD_L));  
+    analogWrite(PWM2, abs(SPD_R));
+
+
+  }
+
+  
+
+  private:
+  int16_t SPD_L = 0;
+  int16_t SPD_R = 0;
+};
+
+  
+
---
+++ MMS/MonsterMotorShield/MonsterMotorShield.ino
@@ -0,0 +1,201 @@
+#include "MMS/MMS.cpp"
+
+/***********SERIAL DEFINES***********/
+// IF cmd_vel not recived during 2 sec => vel = 0
+#define CMD_VEL_TIMEOUT 3000
+
+// Baudrate for serial communication
+#define SERIAL_BAUD 115200
+
+// Rate for serial timeout [ms]
+#define SERIAL_RATE 10 
+/*******END OF SERIAL DEFINES*******/
+
+
+MMS mms;
+
+// Encoder variables
+int64_t PRESENT_EN_L = 0;
+int64_t PRESENT_EN_R = 0;
+int64_t PAST_EN_L = 0;
+int64_t PAST_EN_R = 0;
+
+// Velocity variables
+float PRESENT_VEL_L = 0;
+float PRESENT_VEL_R = 0;
+float PAST_VEL_L = 0;
+float PAST_VEL_R = 0;
+
+// Linear vel and angular vel variables
+float lin = 0.0; // m/s
+float ang = 0.0; // m/s  (+ CWW | - CW)
+
+// Present linear vel and angular vel by encoders
+float pres_lin = 0.0;   // m/s
+float pres_ang = 0.0;   // m/s  (+ CWW | - CW)
+
+// Left motor PID variables
+float PID_L = 0;        // Error
+float I_PID_L = 0;      // Integral Error
+float P_L = 350;        // P coefficient
+float I_L = 60;         // I coefficient
+float D_L = 0;          // D coefficient
+
+// Right motor PID variables
+float PID_R = 0;        // Error  
+float I_PID_R = 0;      // Integral Error
+float P_R = 350;        // P coefficient
+float I_R = 60;         // I coefficient
+float D_R = 0;          // D coefficient
+
+
+// Millis timer
+unsigned long timer;
+
+
+
+void setup(){
+  delay(1000);
+  Serial.begin(SERIAL_BAUD);
+  Serial.setTimeout(SERIAL_RATE);
+  
+  
+  
+  
+  mms.start();
+  
+  // Interrupt for reading encoders
+  attachPCINT(digitalPinToPCINT(EN1A), readLeftEnc, RISING);
+  attachPCINT(digitalPinToPCINT(EN1B), readRightEnc, RISING);
+
+  // Initialize global timer
+  timer = millis();
+  }
+
+
+void loop() {
+        
+    calculate_vel(lin, ang, 0.05);
+    
+    Serial.print(pres_lin);
+    Serial.print(",");
+    Serial.println(pres_lin);
+    Serial.print(pres_ang);
+    Serial.print(",");
+    Serial.println(pres_ang);
+    
+  // Check velocity timeout
+  if(millis() - timer >= CMD_VEL_TIMEOUT){
+    lin = 0.0;
+    ang = 0.0;
+    mms.motor_stop();
+  }
+    delay(50);
+}
+
+
+// Reading encoder PRESENT_EN_L
+void readLeftEnc(){
+  if(digitalRead(EN2A)){
+    PRESENT_EN_L++;
+  }
+  else{
+   PRESENT_EN_L--;
+  }
+}
+
+// Reading encoder PRESENT_EN_R
+void readRightEnc(){
+  if(digitalRead(EN2B)){
+    PRESENT_EN_R++;
+  }
+  else{
+    PRESENT_EN_R--;
+  }
+}
+
+// Recive linear vel, angular vel, frequency of work cycle
+// Calculate encoders speed to velocity of each wheels
+// Check difference between recived vel and encoders vel
+// Tune vel of each wheel with PID, convert answers to PWM
+// Send PWM to Monster Motor Shield 
+void calculate_vel(float lin, float ang, float freq){
+
+  // Calculate encoders speed to velocity of each wheels
+  PRESENT_VEL_L = ((PRESENT_EN_L - PAST_EN_L) * WHEEL_DIAMETER * 3.14) / (TIX_PER_SPIN_L * freq); // m/s
+  PRESENT_VEL_R = ((PRESENT_EN_R - PAST_EN_R) * WHEEL_DIAMETER * 3.14) / (TIX_PER_SPIN_R * freq); // m/s
+
+  // NOT USED YET
+  //if ((PID_L > -10)&&(PID_L < 10)){I_PID_L = 0;}
+  //if ((PID_R > -10)&&(PID_R < 10)){I_PID_R = 0;}
+  // NOT USED YET
+  //if (PRESENT_VEL_L < 0) {P_L = 11;} else {P_L = 10;}
+  //if (PRESENT_VEL_R < 0) {P_R = 11;} else {P_R = 10;}
+
+  // Calculate and encrease Integral part of PID
+  I_PID_L += (lin - (ang*BASE_WIDTH) - PRESENT_VEL_L) * freq * I_L;
+  I_PID_R += (lin + (ang*BASE_WIDTH) - PRESENT_VEL_R) * freq * I_R;
+
+  // Calculate PID
+  PID_L = (lin -(ang*BASE_WIDTH) - PRESENT_VEL_L) * P_L + I_PID_L;
+  PID_R = (lin + (ang*BASE_WIDTH)- PRESENT_VEL_R) * P_R + I_PID_R;
+  PID_L += (PRESENT_VEL_L - PAST_VEL_L) * D_L;
+  PID_R += (PRESENT_VEL_R - PAST_VEL_R) * D_R;
+
+  // Send PWM to Moster Motor Shield
+  mms.set_pwm(PID_L, PID_R);
+
+  //#####################################################//
+  // Calculate present vel and ang
+  pres_lin = (PRESENT_VEL_L + PRESENT_VEL_R) / 2;
+  pres_ang = (PRESENT_VEL_R - PRESENT_VEL_L) / BASE_WIDTH;
+  // TODO Calculate present position
+
+  // Update previous data
+  PAST_VEL_L = PRESENT_VEL_L;
+  PAST_VEL_R = PRESENT_VEL_R;
+  PAST_EN_L = PRESENT_EN_L;
+  PAST_EN_R = PRESENT_EN_R;
+}
+
+void sendSerial_float(float* data, int data_length, String name_){
+  Serial.print(name_+",");
+  for(int i = 0;i < data_length;i++){
+    Serial.print(data[i]);
+    if(i != (data_length-1))
+      Serial.print(",");
+  }
+  Serial.println();
+}
+
+/**********SERIAL FUNCTIONS SPACE***********/
+// Reading and decoding cmd_vel from Serial port
+void serialEvent(){
+  if(Serial.available()){
+    // Buffer for reciving mail from Serial
+    char mail[12]={}; // [+x.xx,+x.xx/n] 12 chars
+    // Reading mail to buffer
+    Serial.readBytesUntil('\n', mail, 12);
+
+    // Local buffer for parts of mail
+    String num;
+    // Variable for splitting mail on 2 parts
+    bool split = true;
+
+    // Mail splitting cycle
+    for(int i=0; i<sizeof(mail); i++){
+      if(mail[i] != ','){
+        num += mail[i];
+      }else{
+        // Read linear velocity
+        lin = num.toFloat();
+        num = "";
+        }
+    }
+    // Read angular velocity
+    ang = num.toFloat();
+    timer = millis();
+  }
+}
+
+//void send
