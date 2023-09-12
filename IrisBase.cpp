65b5d515fde03d6aec0a9019c698d96c3d0ff167
Dmitry <56763340+dmitrypavlikov@users.noreply.github.com>
2023-09-12 17:37:24 +0300

    CalculateBasePos added! TODO serial publishing pos

--- IrisBase.cpp
+++ IrisBase.cpp
@@ -11,11 +11,42 @@ void IrisBase::init(char L_LorR, int L_CWPin, int L_CCWPin,
                     L_PWMPin, L_ENC1Pin, L_ENC2Pin);
     RightMotor.init(R_LorR, R_CWPin, R_CCWPin,
                     R_PWMPin, R_ENC1Pin, R_ENC2Pin);
+
+    GoalVelocityX = 0.0;
+    GoalVelocityY = 0.0;
+    GoalVelocityW = 0.0;
+
+    RealVelocityX = 0.0;
+    RealVelocityY = 0.0;
+    RealVelocityW = 0.0;
+    
+    BasePosX = 0.0;
+    BasePosY = 0.0;
+    BasePosW = 0.0;
+
+    Timer = millis();
 }
 
 void IrisBase::tick() {
     LeftMotor.tick();
     RightMotor.tick();
+
+    if(millis() - Timer > 50) {
+        this->calculateBasePos();
+        Timer = millis();
+    }
+}
+
+void IrisBase::calculateBasePos() {
+    double dx = 0;
+    double dy = 0;
+
+    dx = this->getPresentLinVel() * cos(BasePosW + this->getPresentAngVel()/2);
+    dy = this->getPresentLinVel() * sin(BasePosW + this->getPresentAngVel()/2);
+
+    BasePosX += dx;
+    BasePosY += dy;
+    BasePosW += this->getPresentAngVel();
 }
 
 void IrisBase::setGoalVelocity(double _GoalVelocityX, double _GoalVelocityW) {
@@ -23,15 +54,30 @@ void IrisBase::setGoalVelocity(double _GoalVelocityX, double _GoalVelocityW) {
     RightMotor.setGoalVelocity(_GoalVelocityX, _GoalVelocityW);
 }
 
-double IrisBase::getPresentVelL() {
-    return LeftMotor.getPresentVel();
-}
-
-double IrisBase::getPresentVelR() {
-    return RightMotor.getPresentVel();
+double IrisBase::getPresentLinVel() {
+    return (this->getPresentVelL() + this->getPresentVelR()) / 2;
 }
 
 double IrisBase::getPresentAngVel() {
+    return (this->getPresentVelR() - this->getPresentVelL()) / BASE_WIDTH;
+}
 
+double IrisBase::getPresentVelL() {
+    return this->LeftMotor.getPresentVel();
+}
 
+double IrisBase::getPresentVelR() {
+    return this->RightMotor.getPresentVel();
+}
+
+double IrisBase::getBasePosX() {
+    return this->BasePosX;
+}
+
+double IrisBase::getBasePosY() {
+    return this->BasePosY;
+}
+
+double IrisBase::getBasePosW() {
+    return this->BasePosW;
 }
--- IrisBase.h
+++ IrisBase.h
@@ -6,37 +6,44 @@
 
 class IrisBase {
     private:
-        double GoalVelocityX;
-        double GoalVelocityY;
-		double GoalVelocityW;
+      double GoalVelocityX;
+      double GoalVelocityY;
+		  double GoalVelocityW;
 
-		double RealVelocityX;
-		double RealVelocityY;
-		double RealVelocityW;
+		  double RealVelocityX;
+		  double RealVelocityY;
+		  double RealVelocityW;
 
-        double EncoderPoseX;
-        double EncoderPoseY;
-        double EncoderPoseW;
+      double BasePosX;
+      double BasePosY;
+      double BasePosW;
 
-        unsigned long Timer;
+      unsigned long Timer;
+
+      void calculateBasePos();
 
     public:
-        IrisMotor LeftMotor;
-        IrisMotor RightMotor;
+      IrisMotor LeftMotor;
+      IrisMotor RightMotor;
 
-        void init(char L_LorR, int L_CWPin, int L_CCWPin, 
-                    int L_PWMPin, int L_ENC1Pin, int L_ENC2Pin,
-                    char R_LorR, int R_CWPin, int R_CCWPin,
-                    int R_PWMPin, int R_ENC1Pin, int R_ENC2Pin);
+      void init(char L_LorR, int L_CWPin, int L_CCWPin, 
+                  int L_PWMPin, int L_ENC1Pin, int L_ENC2Pin,
+                  char R_LorR, int R_CWPin, int R_CCWPin,
+                  int R_PWMPin, int R_ENC1Pin, int R_ENC2Pin);
 
-        void tick();
-        
-        void setGoalVelocity(double _GoalVelocityX, double _GoalVelocityW);
-        // Getters
-        double getPresentVelL();
-        double getPresentVelR();
-        double getPresentLinVel();
-        double getPresentAngVel();
+      void tick();
+
+      
+      void setGoalVelocity(double _GoalVelocityX, double _GoalVelocityW);
+
+      double getPresentVelL();
+      double getPresentVelR();
+      double getPresentLinVel();
+      double getPresentAngVel();
+      double getBasePosX();
+      double getBasePosY();
+      double getBasePosW();
+
 
 };
 
